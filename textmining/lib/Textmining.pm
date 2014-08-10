9140754ddabffab476dff5fad3c69e01d0271710
PwnyTail <pwnytail@c3d2.de>
2014-08-10 15:40:05 +0200

    admin interface for creating data -> public structure

--- textmining/lib/Textmining.pm
+++ textmining/lib/Textmining.pm
@@ -24,12 +24,28 @@ sub startup {
     $r->get('/')->to('example#welcome');
 
 
-  # Admin route to controller
-  $r->get('/admin')->to('admin#overview');
-  $r->post('/admin')->to('admin#overview');
+    # Admin route to controller
+    # Admin section
+    # Local logic shared only by routes in this group
+    my $admin = $r->under( '/admin' => sub {
+            #   my $c = shift;
+            #   return 1 if $c->req->headers->header('X-Awesome');
+            #   $c->render(text => "You're not awesome enough.");
+            #   return undef;
+            return 1;
+        });
+    # GET /admin
+    $admin->get()->to('admin#overview');
+    # POST /admin/course?course="foo"&type="bar"
+    $admin->post('/course')->to('admin#course');
+    # POST /admin/open?course="foo"
+    $admin->post('/open')->to('admin#open');
+    # POST /admin/update
+    $admin->post('/update')->to('admin#update');
 
-  # Course route to controller
-  $r->get('/course')->to('course#overview');
+
+    # Course route to controller
+    $r->get('/course')->to('course#overview');
 }
 
 1;
--- textmining/lib/Textmining/Admin.pm
+++ textmining/lib/Textmining/Admin.pm
@@ -3,11 +3,37 @@ use Mojo::Base 'Mojolicious::Controller';
 
 # This action will render a template
 sub overview {
-  my $self = shift;
-
-  # Render template "admin/overview.html.ep" with table
-  my @courses = qw(Textmining foo);
-  $self->render(courses => \@courses);
+    my $self = shift;
+    # Render template "admin/overview.html.ep" with available courses
+    my @courses = $self->struct->get_courses_data();
+    $self->render(courses => \@courses);
 }
 
+sub open {
+    my $self = shift;
+
+    my $course = $self->param('course');
+    use Data::Printer;
+    p $self->struct->get_modules_data($course);
+    $self->redirect_to('/admin');
+}
+sub course {
+    my $self = shift;
+
+    my $course = $self->param('course');
+    my $type = $self->param('type');
+
+    if ($type =~ m/free/) {
+        use Data::Printer;
+        p $self->struct->get_modules_data($course);
+    }
+    $self->redirect_to('/admin');
+}
+
+sub update {
+    my $self = shift;
+
+    $self->struct->update_data_struct();
+    $self->redirect_to('/admin');
+}
 1;
