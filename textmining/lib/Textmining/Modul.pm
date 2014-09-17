56525177dcfd21d9e877b3101ebdaa1cd5a78238
PwnyTail <pwnytail@c3d2.de>
2014-09-17 20:46:20 +0200

    indent fix

--- textmining/lib/Textmining/Modul.pm
+++ textmining/lib/Textmining/Modul.pm
@@ -6,36 +6,36 @@ use Mojo::ByteStream;
 
 # This action will render a template
 sub modul {
-  my $self = shift;
+    my $self = shift;
+    my $course = $self->param('course');
+    my $modul  = $self->param('modul');
+    my $pagenr = $self->param('page') or 0;
 
-  my $course = $self->param('course');
-  my $modul = $self->param('modul');
-  my $pagenr = $self->param('page') or 0;
-  # Render template "modul/modul.html.ep"
-  # TODO create in StrureHelper.pm : 
-  # * get_public_page_path($meta_struct, $modul)
-  my $course_meta_struct  = $self->struct->get_public_struct($course);
-  my @page_path = &_get_public_page_path($course_meta_struct, $modul);
-  my @navbar = &_get_public_navbar($course_meta_struct, $modul);
+    # TODO create in StrureHelper.pm : 
+    # * get_public_page_path($meta_struct, $modul)
+    my $course_meta_struct  = $self->struct->get_public_struct($course);
+    my @page_path   = $self->struct->get_public_page_path($course_meta_struct, $modul);
+    my @navbar      = $self->struct->get_public_navbar($course_meta_struct, $modul);
 
-  # TODO error message
-  $self->redirect_to('/course') unless (@page_path);
-  # TODO
-  # a job for Mojo::Content
-  $pagenr = 0 if ($pagenr >= (@page_path - 1));
-  $pagenr = @page_path - $pagenr - 2 if ($pagenr < 0);
+    # TODO error message
+    $self->redirect_to('/course') unless (@page_path);
+    # TODO
+    # a job for Mojo::Content ? 
+    $pagenr = 0 if ($pagenr >= (@page_path - 1));
+    $pagenr = @page_path - $pagenr - 2 if ($pagenr < 0);
 
-  my $file      = Mojo::Asset::File->new( path => $page_path[$pagenr]);
-  my $stream    = Mojo::ByteStream->new($file->slurp)->decode('UTF-8');
+    my $file      = Mojo::Asset::File->new( path => $page_path[$pagenr]);
+    my $stream    = Mojo::ByteStream->new($file->slurp)->decode('UTF-8');
 
-  $self->render(
-      course        =>  $course,
-      modul         =>  $modul,
-      navbar        =>  \@navbar,
-      page          =>  $stream,
-      pagenr        =>  $pagenr,
-      page_path     =>  \@page_path
-  );
+    # Render template "modul/modul.html.ep"
+    $self->render(
+        course        =>  $course,
+        modul         =>  $modul,
+        navbar        =>  \@navbar,
+        page          =>  $stream,
+        pagenr        =>  $pagenr,
+        page_path     =>  \@page_path
+    );
 }
 
 sub _get_public_page_path ($$$) {
